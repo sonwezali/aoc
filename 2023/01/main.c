d0ed37e14c32b2d97590f0c56f3814f143092cfe
sonwezali <al1.sonmez.mi@gmail.com>
2024-01-16 20:02:11 +0300

    updated /2023/01/main.c

--- 2023/01/main.c
+++ 2023/01/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
+#define MAX_SIZE 1000
+
+int isNumeric(char c)
+{
+    return c >= '0' && c <= '9';
+}
 
 int main()
 {
-    printf("Hello World!\n");
+    FILE *file;
+    char line[MAX_SIZE];
+
+    file = fopen("input.txt", "r");
+    
+    if (file == NULL)
+    {
+        printf("Error: Could not open file\n");
+        return 1;
+    }   
+
+    /*while (fgets(line, MAX_SIZE, file) != NULL)
+    {
+        printf("%s", line);
+    }*/
+
+    fgets(line, MAX_SIZE, file);
+    fgets(line, MAX_SIZE, file);
+
+    char firstNum = '\0';
+    char lastNum = '\0';
+
+    for (int i = 0; line[i] != '\0'; i++)
+    {
+        if (isNumeric(line[i])) 
+        {
+            if (firstNum == '\0') 
+            {
+                firstNum = line[i]; 
+            }
+            else 
+            {
+                lastNum = line[i];
+            }
+        }
+    }
+
+    fclose(file);
     
     return 0;
 }
